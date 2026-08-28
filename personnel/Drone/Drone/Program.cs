7fd3131d49e9e16c874a34963d24cc3b2d989cf3
Naël Vuille-Bille <ps00xbh@eduvaud.ch>
2026-08-28 12:15:45 +0200

    multi drone qui marche pas

--- personnel/Drone/Drone/Program.cs
+++ personnel/Drone/Drone/Program.cs
@@ -18,48 +18,62 @@ namespace Drone
 
             //déclaration variable
             int posx = 0;//position x du drone
-            int bat = 50;//pourcentage de la battrie
+            int nb_drone = 20;
+            int[] bat = new int[nb_drone];//pourcentage de la battrie
+            Random random = new Random();
+
+            for(int i = 0;i < nb_drone-1; i++)
+            {
+                bat[i] = random.Next(2, 100);
+            }
+            
+
+
 
             //boucle du drone qui avance
             do
             {
                 Console.Clear();
                 Etat(ref posx, ref bat);
-                Drone(ref posx, ref bat, posy);
+                Drone(ref posx, ref bat, posy,nb_drone);
                 Thread.Sleep(150);
-            } while (bat > 0);
+            } while (1 > 0);
 
             //affiche le drone mort
             Console.Clear();
-            Drone(ref posx, ref bat, posy);
+            Drone(ref posx, ref bat, posy,nb_drone);
             Console.ReadLine();
 
         }
         //Etat du drone
-        static void Etat (ref int posx, ref int bat)
+        static void Etat (ref int posx, ref int[] bat)
         {
-            if (bat > 0) { 
-                posx += 1;
-                bat -= 2;
+            for (int i = 0; ; i++) { 
+                if (bat[i] > 0) { 
+                    posx += 1;
+                    bat[i] -= 2;
+                }
             }
         }
         //dessin du drone
-        static void Drone(ref int posx,ref int bat,int posy)
+        static void Drone(ref int posx,ref int[] bat,int posy,int nb_drone)
         {
-            
-            Console.SetCursorPosition(posx, posy);
-            if (bat > 0)
+            for (int i = 0; i < nb_drone; i++)
             {
-                Console.WriteLine("x-O-x");
-        
+                Console.SetCursorPosition(posx, posy+i);
+                if (bat[i] > 0)
+                {
+                    Console.WriteLine("x-O-x");
+
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("_____");
+                }
+                Console.SetCursorPosition(posx + 1, posy + i);
+                Console.WriteLine(bat[i] + "%");
             }
-            else
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("_____");
-            }
-            Console.SetCursorPosition(posx+1, posy+1);
-            Console.WriteLine(bat + "%");
         }
     }
 }
